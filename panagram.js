b9a8d10a4e2c93fc9996f8064699ebb1cbf8d754
Sai Krishna Buddavarapu <93121782+BuddavarapuSaikrishna@users.noreply.github.com>
2025-09-07 16:58:14 +0530

    Update panagram.js

--- panagram.js
+++ panagram.js
@@ -1,3 +1,22 @@
+                          // Panagram
+
+/*
+Given a word or sentence and a string of lowercase letters, determine if the word or sentence uses all the letters from the given set at least once and no other letters.
+
+Ignore non-alphabetical characters in the word or sentence.
+Ignore letter casing in the word or sentence.
+
+isPangram("hello", "helo") should return true
+Waiting:2. isPangram("hello", "hel") should return false
+Waiting:3. isPangram("hello", "helow") should return false
+Waiting:4. isPangram("hello world", "helowrd") should return true
+Waiting:5. isPangram("Hello World!", "helowrd") should return true
+Waiting:6. isPangram("Hello World!", "heliowrd") should return false
+Waiting:7. isPangram("freeCodeCamp", "frcdmp") should return false
+Waiting:8. isPangram("The quick brown fox jumps over the lazy dog.", "abcdefghijklmnopqrstuvwxyz") should return true
+
+*/
+
 function isPangram(sentence, letters) {
 
   let arr = []
